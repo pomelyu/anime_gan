eb674398f2ba9505f35a6c6026f6c9fcc3060b5a
pomelyu <pomelyu5199@icloud.com>
2018-09-16 23:45:46 +0800

    :art: format the code to fulfill pylint and pytest

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -1,34 +1,43 @@
+# pylint: disable=invalid-name
 from torch.utils.data import DataLoader
 import torch
 
-import context
+import context # pylint: disable=unused-import
 from dataset import AnimeData, NoiseData
 
+# ============================= #
 # Test arguments
 # ============================= #
 DATA_PATH = "data/faces"
 BATCH_SIZE = 16
 NOISE_SIZE = 100
+
+
 # ============================= #
+# Testing
+# ============================= #
+def test_anime_data():
+    anime_data = AnimeData(DATA_PATH)
+    anime_dataloader = DataLoader(anime_data, batch_size=BATCH_SIZE, shuffle=True)
 
-# Test Anime data
-anime_data = AnimeData(DATA_PATH)
-anime_dataloader = DataLoader(anime_data, batch_size=BATCH_SIZE, shuffle=True)
+    anime_iter = iter(anime_dataloader)
+    images = next(anime_iter)
 
-anime_iter = iter(anime_dataloader)
-images = next(anime_iter)
+    assert images.shape == torch.Tensor(BATCH_SIZE, 3, 64, 64).shape
+    assert torch.max(images) <= 1 and torch.min(images) >= 0
 
-assert(images.shape == torch.Tensor(BATCH_SIZE, 3, 64, 64).shape)
-assert(torch.max(images) <= 1)
-assert(torch.min(images) >= 0)
 
-# Test Noise data
-noise_data = NoiseData(NOISE_SIZE, len(anime_data))
-noise_dataloader = DataLoader(noise_data, batch_size=BATCH_SIZE)
+def test_noise_data():
+    noise_data = NoiseData(NOISE_SIZE, BATCH_SIZE)
+    noise_dataloader = DataLoader(noise_data, batch_size=BATCH_SIZE)
 
-noise_iter = iter(noise_dataloader)
-noises = next(noise_iter)
+    noise_iter = iter(noise_dataloader)
+    noises = next(noise_iter)
 
-assert(noises.shape == torch.Tensor(BATCH_SIZE, NOISE_SIZE, 1, 1).shape)
-assert(torch.max(noises) <= 1)
-assert(torch.min(noises) >= 0)
+    assert noises.shape == torch.Tensor(BATCH_SIZE, NOISE_SIZE, 1, 1).shape
+    assert torch.max(noises) <= 1 and torch.min(noises) >= 0
+
+
+if __name__ == "__main__":
+    test_anime_data()
+    test_noise_data()
--- test/test_model.py
+++ test/test_model.py
@@ -1,29 +1,31 @@
+# pylint: disable=invalid-name
 import torch
 from torch.utils.data import DataLoader
 import numpy as np
 
-import context
-from dataset import AnimeData, NoiseData
+import context # pylint: disable=unused-import
+from dataset import NoiseData
 from models import NetG, NetD
 
+# ============================= #
 # Test arguments
 # ============================= #
 DATA_PATH = "data/faces"
 BATCH_SIZE = 16
 NOISE_SIZE = 100
-# ============================= #
 
-# Prepare
+# ============================= #
+# Setup
+# ============================= #
+class Opt():
+    noise_size = NOISE_SIZE
+opt = Opt()
+
 noise_data = NoiseData(NOISE_SIZE, BATCH_SIZE)
 noise_dataloader = DataLoader(noise_data, batch_size=BATCH_SIZE)
 noise_iter = iter(noise_dataloader)
 
 feature_map = next(noise_iter)
-
-class Opt():
-    noise_size = NOISE_SIZE
-
-opt = Opt()
 
 net_G = NetG(opt)
 net_D = NetD()
@@ -31,44 +33,59 @@ net_D = NetD()
 criterion = torch.nn.MSELoss()
 optimzer = torch.optim.SGD(net_D.parameters(), lr=0.1)
 
-# Test
-generated = net_G(feature_map)
-assert(generated.shape == torch.Tensor(BATCH_SIZE, 3, 64, 64).shape)
-assert(torch.max(generated) <= 1)
-assert(torch.min(generated) >= 0)
 
-res_generated = net_D(generated)
-assert(res_generated.shape == torch.Tensor(BATCH_SIZE).shape)
-assert(torch.max(res_generated) <= 1)
-assert(torch.max(res_generated) >= 0)
+# ============================= #
+# Testing
+# ============================= #
+def test_networks():
+    generated = net_G(feature_map)
+    assert generated.shape == torch.Tensor(BATCH_SIZE, 3, 64, 64).shape
+    assert torch.max(generated) <= 1
+    assert torch.min(generated) >= 0
 
-# Test: freeze params
-net_D.set_requires_grad(False)
-ori_params = []
-for param in net_D.parameters():
-    ori_params.append(param.detach().numpy().copy())
-    assert param.requires_grad == False
-
-loss = criterion(res_generated, torch.ones(BATCH_SIZE, dtype=torch.float32))
-loss.backward()
-optimzer.step()
-
-for i, param in enumerate(net_D.parameters()):
-    assert np.array_equal(param.detach().numpy(), ori_params[i])
+    res_generated = net_D(generated)
+    assert res_generated.shape == torch.Tensor(BATCH_SIZE).shape
+    assert torch.max(res_generated) <= 1
+    assert torch.max(res_generated) >= 0
 
 
-# Test: Unfreeze params
-ori_params = []
-net_D.set_requires_grad(True)
-for param in net_D.parameters():
-    ori_params.append(param.detach().numpy().copy())
-    assert param.requires_grad == True
+def test_freeze_grad():
+    generated = net_G(feature_map)
+    res_generated = net_D(generated)
 
-generated = net_G(feature_map)
-res_generated = net_D(generated)
-loss = criterion(res_generated, torch.ones(BATCH_SIZE, dtype=torch.float32))
-loss.backward()
-optimzer.step()
+    net_D.set_requires_grad(False)
+    ori_params = []
+    for param in net_D.parameters():
+        ori_params.append(param.detach().numpy().copy())
+        assert param.requires_grad is False
 
-for i, param in enumerate(net_D.parameters()):
-    assert np.array_equal(param.detach().numpy(), ori_params[i]) == False
+    loss = criterion(res_generated, torch.ones(BATCH_SIZE, dtype=torch.float32))
+    loss.backward()
+    optimzer.step()
+
+    for i, param in enumerate(net_D.parameters()):
+        assert np.array_equal(param.detach().numpy(), ori_params[i])
+
+
+def test_unfreeze_grad():
+    ori_params = []
+    net_D.set_requires_grad(True)
+    for param in net_D.parameters():
+        ori_params.append(param.detach().numpy().copy())
+        assert param.requires_grad is True
+
+    generated = net_G(feature_map)
+    res_generated = net_D(generated)
+
+    loss = criterion(res_generated, torch.ones(BATCH_SIZE, dtype=torch.float32))
+    loss.backward()
+    optimzer.step()
+
+    for i, param in enumerate(net_D.parameters()):
+        assert np.array_equal(param.detach().numpy(), ori_params[i]) is False
+
+
+if __name__ == "__main__":
+    test_networks()
+    test_freeze_grad()
+    test_unfreeze_grad()
