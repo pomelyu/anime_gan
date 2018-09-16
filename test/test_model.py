d47c3fbdb07cd77e89c6a6b37753fe028d882dd5
pomelyu <pomelyu5199@icloud.com>
2018-09-16 22:03:30 +0800

    :white_check_mark: test to freeze model paramter

--- test/test_model.py
+++ test/test_model.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import DataLoader
+import numpy as np
 
 import context
 from dataset import AnimeData, NoiseData
@@ -24,11 +25,13 @@ class Opt():
 
 opt = Opt()
 
-
-# Test
 net_G = NetG(opt)
 net_D = NetD()
 
+criterion = torch.nn.MSELoss()
+optimzer = torch.optim.SGD(net_D.parameters(), lr=0.1)
+
+# Test
 generated = net_G(feature_map)
 assert(generated.shape == torch.Tensor(BATCH_SIZE, 3, 64, 64).shape)
 assert(torch.max(generated) <= 1)
@@ -38,3 +41,34 @@ res_generated = net_D(generated)
 assert(res_generated.shape == torch.Tensor(BATCH_SIZE).shape)
 assert(torch.max(res_generated) <= 1)
 assert(torch.max(res_generated) >= 0)
+
+# Test: freeze params
+net_D.set_requires_grad(False)
+ori_params = []
+for param in net_D.parameters():
+    ori_params.append(param.detach().numpy().copy())
+    assert param.requires_grad == False
+
+loss = criterion(res_generated, torch.ones(BATCH_SIZE, dtype=torch.float32))
+loss.backward()
+optimzer.step()
+
+for i, param in enumerate(net_D.parameters()):
+    assert np.array_equal(param.detach().numpy(), ori_params[i])
+
+
+# Test: Unfreeze params
+ori_params = []
+net_D.set_requires_grad(True)
+for param in net_D.parameters():
+    ori_params.append(param.detach().numpy().copy())
+    assert param.requires_grad == True
+
+generated = net_G(feature_map)
+res_generated = net_D(generated)
+loss = criterion(res_generated, torch.ones(BATCH_SIZE, dtype=torch.float32))
+loss.backward()
+optimzer.step()
+
+for i, param in enumerate(net_D.parameters()):
+    assert np.array_equal(param.detach().numpy(), ori_params[i]) == False
