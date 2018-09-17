62c3536c56c8bcf7807af81a228b7aa1114b5b1c
pomelyu <pomelyu5199@icloud.com>
2018-09-17 22:38:20 +0800

    :sparkles: use fire to parse command line argumentss

---
+++ config/config.py
@@ -0,0 +1,26 @@
+import warnings
+
+class DefaultConfig(object):
+    use_gpu = False
+    noise_size = 100
+
+    data_path = "data/faces"
+    out_path = "out"
+
+    max_epochs = 5
+    batch_size = 16
+    lr = 0.001
+
+    def _parse(self, kwargs):
+        for k, v in kwargs.items():
+            if not hasattr(self, k):
+                warnings.warn("Warning: opt has no attribute", k)
+            else:
+                setattr(self, k, v)
+
+        print("use config")
+        for k, _ in self.__class__.__dict__.items():
+            if not k.startswith("_"):
+                print(k, ":", getattr(self, k))
+
+opt = DefaultConfig()
